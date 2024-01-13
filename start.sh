b33446b14a2ef2799c53c74e3ac5b48b0f1ac46a
escottalexander@gmail.com <escottalexander@gmail.com>
2024-01-12 21:18:46 -0500

    fix start script and add kill script

--- start.sh
+++ start.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-nohup node server.js > /dev/null 2>~/sms-subscription-service/log &
+# create the log directory if it doesn't exist
+mkdir -p ~/sms-subscription-service
+nohup node ./build/src/index.js 3000 > /dev/null 2>~/sms-subscription-service/log &
+echo $! > pid
+echo "Started SMS Subscription Service"
+
